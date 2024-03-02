a26850ab080aa84cee303b3feed8fd77c9afeda8
MorningTZH <morningtzh@yeah.net>
2024-03-02 17:10:54 +0800

    display ok

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
+
 fn main() {
     embuild::espidf::sysenv::output();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,103 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+//! SPI example with the ST7789 using the ESP-RUST-BOARD
+//!
+//! Folowing pins are used:
+//! RST       GPIO3
+//! DC        GPIO4
+//! BACKLIGHT GPIO5
+//! SCLK      GPIO6
+//! SDA       GPIO7
+//!
+//! Depending on your target and the board you are using you have to change the pins.
+//!
+//! For this example you need to hook up an ST7789 SPI display.
+//! The display will display an image on ferris the crab on a black background.
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+use std::thread;
+use std::time::Duration;
 
-    log::info!("Hello, world!");
+use embedded_hal::spi::MODE_3;
+
+use esp_idf_hal::delay::Ets;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::spi::*;
+use esp_idf_hal::units::FromValueType;
+
+use display_interface_spi::SPIInterface;
+use ili9341::{Ili9341, Orientation};
+
+use embedded_graphics::image::*;
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+
+// use mipidsi::{Builder, Orientation};
+
+fn main() -> anyhow::Result<()> {
+    let peripherals = Peripherals::take()?;
+    let spi = peripherals.spi2;
+
+    let rst = PinDriver::output(peripherals.pins.gpio33)?;
+    let dc = PinDriver::output(peripherals.pins.gpio27)?;
+    let mut backlight = PinDriver::output(peripherals.pins.gpio32)?;
+    let sclk = peripherals.pins.gpio18;
+    let sda = peripherals.pins.gpio23;
+    // let sdi = peripherals.pins.gpio8;
+    let cs = peripherals.pins.gpio14;
+
+    let mut delay = Ets;
+
+    // configuring the spi interface, note that in order for the ST7789 to work, the data_mode needs to be set to MODE_3
+    let config = config::Config::new()
+        .baudrate(26.MHz().into())
+        .data_mode(MODE_3);
+
+    let device = SpiDeviceDriver::new_single(
+        spi,
+        sclk,
+        sda,
+        Option::<Gpio19>::None,
+        Some(cs),
+        &SpiDriverConfig::new(),
+        &config,
+    )?;
+
+    // display interface abstraction from SPI and DC
+    let di = SPIInterface::new(device, dc);
+
+    log::info!("Set ILI9341");
+    let mut display = match Ili9341::new(
+        di,
+        rst,
+        &mut Ets,
+        Orientation::Landscape,
+        ili9341::DisplaySize320x480,
+    ) {
+        Ok(d) => d,
+        Err(err) => {
+            log::error!("new Ili9341 failed: {:?}", err);
+            return Ok(());
+        }
+    };
+
+    log::info!("Clear ILI9341");
+    display.clear_screen(0x5555).expect("ok");
+
+    // turn on the backlight
+    backlight.set_high()?;
+
+    let raw_image_data = ImageRawLE::new(include_bytes!("../ferris.raw"), 86);
+    let ferris = Image::new(&raw_image_data, Point::new(0, 0));
+
+    // draw image on black background
+    ferris.draw(&mut display).unwrap();
+
+    println!("Image printed!");
+
+    
+
+    loop {
+        thread::sleep(Duration::from_millis(1000));
+
+
+    }
 }
